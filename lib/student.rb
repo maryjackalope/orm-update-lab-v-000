78fa16e254a64da7b2d7196f957b102d5289f8b7
Mary Jack <maryjackinthebox@gmail.com>
2018-06-25 20:18:04 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -29,12 +29,27 @@ attr_reader :id
     DB[:conn].execute(sql)
   end
 
+ def save
+    if self.id 
+      self.update
+    else
+      sql = <<-SQL
+        INSERT INTO students (name, grade) 
+        VALUES (?, ?)
+      SQL
+ 
+      DB[:conn].execute(sql, self.name, self.grade)
+      @id = DB[:conn].execute("SELECT last_insert_rowid() FROM songs")[0][0]
+    end
+  end 
+  
   def self.create(name:, grade:)
     student = Student.new(name, grade)
     student.save
     student
   end
  
+  
  def new_from_db
  end
  
