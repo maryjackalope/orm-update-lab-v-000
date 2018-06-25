53b07a9813c0073b8b5f909fd49b6b999983881a
Mary Jack <maryjackinthebox@gmail.com>
2018-06-25 20:20:13 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -55,7 +55,7 @@ attr_reader :id
  
   def self.find_by_name(id)
     sql = "SELECT * FROM students WHERE name = ?"
-    result = DB[:conn].execute(sql, id)[0]
+    result = DB[:conn].execute(sql, name)[0]
     Student.new(result[0], result[1], result[2])
   end
  
